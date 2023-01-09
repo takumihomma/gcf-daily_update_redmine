79bfd6578a87382cb7a18259a185f8dba17be1f3
takumihomma <honma@office-honma.com>
2023-01-09 10:30:19 +0900

    年号の6桁対応

--- README.md
+++ README.md
@@ -11,12 +11,20 @@ main.py ->
 1. Githubにアップロード
     * vscodeで
 1. deploy in Google Cloud SHELL GPCにログインし、クラウドSHELLにて以下の操作をする！
-```
-git clone https://github.com/takumihomma/
-gcf-daily_update_redmine　（最初だけ。Gitに同期したあと、そのファイルをGCPにアップロード）
-cd gcf-daily_update_redmine
-git pull
-sh deploy.sh
-```
 
+    1. 一番最初だけ（2回目以降は不要）
+    ```
+    git clone https://github.com/takumihomma/gcf-daily_update_redmine
+    （最初だけ。Gitに同期したあと、そのファイルをGCPにアップロード）
+    ```
+    1. 毎回行う
+    ```
+    cd gcf-daily_update_redmine
+    git pull
+    sh deploy.sh  -> デプロイに時間がかかる
+    ```
+    1. デプロイが終わったら正常稼働を確認する
+
+    1. 不要になった古いコンテナイメージを削除する
+        1. [Container Registory](https://console.cloud.google.com/gcr/images/zsol22/asia/gcf/asia-northeast2/92df3395-3fc3-400e-b7c3-ed244feca1d6?hl=ja&project=zsol22)にアクセスして古いものを削除
 
--- main.py
+++ main.py
@@ -191,17 +191,14 @@ def login_redmine(driver, url, username, password):
 
 #%% -------Duplicate Issue--------
 def duplicate_issue(driver, page,start_dateY, start_dateM, start_dateD, due_dateY, due_dateM, due_dateD):
-    # 前回の作成で年が「2022」にならず「0002」になってしまうバグがある。年が1000未満の場合にはチケットが作成できないため、2020を加算する
-    #if start_dateY < 2020:
-    #    start_dateY = start_dateY + 2020
-    #if due_dateY < 2020:
-    #    due_dateY = due_dateY + 2020
+    # <input type="date">タグは年号を6桁で設定する必要がある。
 
     driver.get(page)
     WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located)
     target = driver.find_element(by=By.ID, value='issue_start_date')
+    target.send_keys("00")
     target.send_keys(start_dateY)
-    target.send_keys(Keys.TAB)
+    #target.send_keys(Keys.TAB) # 6桁で入力するときはTABキーは不要
     target.send_keys(start_dateM)
     target.send_keys(start_dateD)
     target = driver.find_element(by=By.ID, value='issue_due_date')
